9cbdf2114e20275af4305e6b1c617d6577266d3f
ziming <ziming@users.noreply.github.com>
2025-07-31 02:35:36 +0800

    See if this fix the flow

--- src/Factories/ComponentParameterFactory.php
+++ src/Factories/ComponentParameterFactory.php
@@ -28,19 +28,21 @@ final class ComponentParameterFactory
     }
 
     /**
-     * @param  string  $flowToken  The flow token used as an identifier defaults to unused. Used when sub_type is flow.
-     * @param  array  $flowActionData  A json object with the data payload for the first screen. Used when sub_type is flow.
-     * @param  string|null  $thumbnailProductRetailerId  Item SKU number, labeled as Content ID in the Commerce Manager. Required when sub_type is catalog.
+     * @param  ?string  $flowToken  The flow token used as an identifier defaults to unused. Used when sub_type is flow.
+     * @param  ?array  $flowActionData  A json object with the data payload for the first screen. Used when sub_type is flow.
+     * @param  ?string  $thumbnailProductRetailerId  Item SKU number, labeled as Content ID in the Commerce Manager. Required when sub_type is catalog.
      */
-    public static function buttonFlow(string $flowToken, array $flowActionData, ?string $thumbnailProductRetailerId = null): array
+    public static function buttonFlow(?string $flowToken = null, ?array $flowActionData = [], ?string $thumbnailProductRetailerId = null): array
     {
         return [
-            'type' => 'action',
-            'action' => [
-                'thumbnail_product_retailer_id' => $thumbnailProductRetailerId,
-                'flow_token' => $flowToken,
-                'flow_action_data' => $flowActionData,
-            ],
+            [
+                'type' => 'action',
+                'action' => [
+                    'thumbnail_product_retailer_id' => $thumbnailProductRetailerId,
+                    'flow_token' => $flowToken,
+                    'flow_action_data' => $flowActionData,
+                ],
+            ]
         ];
     }
 }
