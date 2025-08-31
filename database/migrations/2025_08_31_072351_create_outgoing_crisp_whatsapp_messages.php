4b31159e1e59acdef97d0f3b61de5a7275555317
ziming <679513+ziming@users.noreply.github.com>
2025-08-31 08:14:50 +0000

    Fix styling

--- database/migrations/2025_08_31_072351_create_outgoing_crisp_whatsapp_messages.php
+++ database/migrations/2025_08_31_072351_create_outgoing_crisp_whatsapp_messages.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
-        Schema::create('outgoing_crisp_whatsapp_messages', function (Blueprint $table) {
+        Schema::create('outgoing_crisp_whatsapp_messages', function (Blueprint $table): void {
             $table->id();
             $table->uuid('request_id')->unique()->nullable();
             $table->boolean('error');
