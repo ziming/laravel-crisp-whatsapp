0b6b2f76c3c0032af6ff4871f1f39574ec74f1fc
ziming <ziming@users.noreply.github.com>
2025-09-12 01:31:29 +0800

    add support for prune command

--- src/Models/CrispWhatsAppMessageLog.php
+++ src/Models/CrispWhatsAppMessageLog.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 namespace Ziming\LaravelCrispWhatsApp\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
 
 class CrispWhatsAppMessageLog extends Model
 {
+    use Prunable;
+
     protected $table = 'crisp_whatsapp_message_logs';
 
     protected $guarded = [
@@ -27,5 +32,13 @@ class CrispWhatsAppMessageLog extends Model
             'callback_response_error' => 'boolean',
             'callback_response_data' => 'array',
         ];
+    }
+
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<', Carbon::now()->subDays(
+                config('crisp-whatsapp.delete_records_older_than_days')
+            )
+        );
     }
 }
